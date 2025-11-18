2f53743d020beddbc436869e6b877a160377dc8e
Dieneffer-Lima <dieneffer.lima@estudante.uffs.edu.br>
2025-11-18 12:51:49 -0300

    Add files via upload

--- consultas/consultas.sql
+++ consultas/consultas.sql
@@ -1,4 +1,8 @@
 --consulta 1, Listagem completa de vendas
+-- Lista todas as vendas realizadas no sistema
+-- JOIN com cliente para mostrar o nome do cliente
+-- JOIN com usuario para mostrar quem registrou a venda
+-- Ordena da venda mais recente para a mais antiga
 SELECT
     v.id_venda,
     v.data_venda,
@@ -13,6 +17,10 @@ JOIN usuario u   ON u.id_usuario = v.id_usuario
 ORDER BY v.data_venda DESC;
 
 --consulta 2, Faturamento Bruto Mensal
+-- Calcula o total vendido por mês
+-- date_trunc('month') pega apenas mês/ano
+-- SUM soma o valor total das vendas de cada mês
+-- GROUP BY agrupa por mês
 SELECT
     date_trunc('month', data_venda) AS mes,
     SUM(valor_total_venda) AS total_vendido
@@ -21,6 +29,10 @@ GROUP BY mes
 ORDER BY mes;
 
 --consulta 3, Total de Despesas por Categoria
+-- Soma o valor das despesas agrupando por categoria
+-- WHERE filtra o período 
+-- GROUP BY separa as categorias 
+-- ORDER BY mostra a categoria que mais gastou primeiro
 SELECT
     categoria,
     SUM(valor_despesa) AS total_despesa
@@ -30,6 +42,10 @@ GROUP BY categoria
 ORDER BY total_despesa DESC;
 
 --consulta 4, Lucro Líquido Mensal (Vendas – Despesas)
+-- Subconsulta v, soma total das vendas por dia
+-- Subconsulta d, soma total das despesas por dia
+-- LEFT JOIN, liga vendas e despesas pela data
+-- Cálculo, lucro líquido = total_vendas - total_despesas
 SELECT
     v.data_venda,
     v.total_vendas,
@@ -55,6 +71,8 @@ ON v.data_venda = d.data_despesa
 ORDER BY v.data_venda;
 
 --consulta 5, Número de Vendas em um Período
+-- Conta quantas vendas foram feitas em cada dia
+-- Agrupado por data e ordenado em ordem cronológica
 SELECT
     data_venda,
     COUNT(*) AS numero_vendas
